933f43da34861ceb9e32a78daca560754630cdad
Zindo Yamate <congvn00682@gmail.com>
2018-05-12 12:16:37 +0700

    Initial Commit

---
+++ FoodOrder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodOrder
+//
+//  Created by Zindo Yamate on 5/12/18.
+//  Copyright © 2018 Zindo Yamate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
